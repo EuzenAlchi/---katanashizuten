6d2e302dde24bd6c3b36b76d1907b0b7c4b0643d
Naxtroon <107158145+Naxtroon@users.noreply.github.com>
2022-06-08 20:55:50 -0500

    primer componente
    Tuto angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './component/principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"gremio",component:PrincipalComponent},{path:"**",redirectTo:"gremio"}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
