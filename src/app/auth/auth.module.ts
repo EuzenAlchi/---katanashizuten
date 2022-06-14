dc79a15ace1660d7dee48520941e3970afc71333
Santiago Alejandro Gaviria Murcia <69436478+santialejo97@users.noreply.github.com>
2022-06-13 21:15:47 -0500

    inicio login
    se montaron estilos y html para el login se creo funcionalidad de botones para mostrar el login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,12 +6,14 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterAdminComponent } from './register-admin/register-admin.component';
 import { RegisterAventureroComponent } from './register-aventurero/register-aventurero.component';
+import { CloudsComponent } from './components/clouds/clouds.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
     RegisterAdminComponent,
     RegisterAventureroComponent,
+    CloudsComponent,
   ],
   imports: [CommonModule, AuthRoutingModule],
 })
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  mostrarLogin() {
+    const loginButton = document.querySelector('#login-button');
+    const loginContainer = document.querySelector('#login-container');
+    console.log('mostrando login');
+    loginButton?.classList.add('animate__fadeOut');
+    loginContainer?.classList.add('animate__fadeInDown');
+    setTimeout(() => {
+      loginContainer?.classList.remove('ocultar');
+    }, 1000);
   }
-
 }
